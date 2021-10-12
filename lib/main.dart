eac62ff642e08af26d4d94cd2fe3c097d4856a6c
Sean Connolly <sconnolly17@gmail.com>
2021-10-12 06:03:37 -0600

    Starting code

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_communities/providers/auth.dart';
-import 'package:flutter_communities/screens/community_screen.dart';
-import 'package:flutter_communities/screens/create_community_screen.dart';
-import 'package:flutter_communities/screens/create_post_screen.dart';
-import 'package:flutter_communities/screens/edit_profile_screen.dart';
-import 'package:flutter_communities/screens/error_demo_screen.dart';
-import 'package:flutter_communities/screens/login_screen.dart';
-import 'package:flutter_communities/screens/post_screen.dart';
-import 'package:flutter_communities/screens/user_profile_screen.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'screens/home_screen.dart';
-import 'screens/registration_screen.dart';
 
 void main() async {
-  await dotenv.load();
-  final container = ProviderContainer();
-  final auth = container.read(authProvider.notifier);
-  await auth.initialize();
-
-  runApp(
-    UncontrolledProviderScope(
-      child: MyApp(),
-      container: container,
-    ),
-  );
+  runApp(FlutterCommunitiesApp());
 }
 
-class MyApp extends StatelessWidget {
+class FlutterCommunitiesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter + GraphQL',
+      title: 'Flutter Communities',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       routes: {
         HomeScreen.routeName: (_) => HomeScreen(),
-        RegistrationScreen.routeName: (_) => RegistrationScreen(),
-        LoginScreen.routeName: (_) => LoginScreen(),
-        CreateCommunityScreen.routeName: (_) => CreateCommunityScreen(),
-        CommunityScreen.routeName: (context) => CommunityScreen(
-            ModalRoute.of(context)!.settings.arguments as String),
-        CreatePostScreen.routeName: (context) => CreatePostScreen(
-            ModalRoute.of(context)!.settings.arguments as String),
-        PostScreen.routeName: (context) => PostScreen(
-            ModalRoute.of(context)!.settings.arguments as PostScreenArguments),
-        ErrorDemoScreen.routeName: (_) => ErrorDemoScreen(),
-        UserProfileScreen.routeName: (_) => UserProfileScreen(),
-        EditProfileScreen.routeName: (_) => EditProfileScreen(),
       },
     );
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,127 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_communities/models/community.dart';
-import 'package:flutter_communities/providers/auth.dart';
-import 'package:flutter_communities/widgets/query_stream.dart';
-import 'package:flutter_communities/widgets/user_app_bar.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_communities/providers/community_repository.dart';
 
-import 'community_screen.dart';
-import 'create_community_screen.dart';
-import 'login_screen.dart';
-import 'registration_screen.dart';
-
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends StatelessWidget {
   static const routeName = '/';
 
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, ScopedReader watch) {
-    final _auth = watch(authProvider);
-
-    return Scaffold(
-      appBar: UserAppBar(),
-      body: _auth.isAuthenticated ? _CommunityList() : _Welcome(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Navigator.of(context).pushNamed(CreateCommunityScreen.routeName);
-        },
-      ),
-    );
-  }
-}
-
-class _Welcome extends StatelessWidget {
-  const _Welcome({Key? key}) : super(key: key);
-
-  @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(32.0),
-            child: Text(
-              'Welcome to Flutter Communities',
-              style: TextStyle(fontSize: 20),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            alignment: Alignment.center,
-            child: Text(
-              '🏡',
-              style: TextStyle(fontSize: 144),
-            ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(RegistrationScreen.routeName);
-              },
-              child: Text('Register'),
-            ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            child: TextButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(LoginScreen.routeName);
-              },
-              child: Text('Login'),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class _CommunityList extends ConsumerWidget {
-  const _CommunityList({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context, ScopedReader watch) {
-    final repository = watch(communityRepositoryProvider);
-
-    return QueryStream<List<Community>?>(
-      stream: repository.getCommunities(),
-      builder: (context, communities, _) {
-        if (communities != null && communities.isEmpty == false) {
-          return ListView.separated(
-              itemBuilder: (context, i) {
-                final community = communities[i];
-                return ListTile(
-                  leading: Text(community.icon, style: TextStyle(fontSize: 24)),
-                  title: Text(community.name),
-                  onTap: () {
-                    Navigator.of(context).pushNamed(CommunityScreen.routeName,
-                        arguments: community.id);
-                  },
-                );
-              },
-              separatorBuilder: (context, i) => const Divider(),
-              itemCount: communities.length);
-        }
-
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 16),
-                child: Text('No Communities', style: TextStyle(fontSize: 24)),
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+        'Flutter Communities',
+      )),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Text(
+                'Welcome to Flutter Communities',
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.center,
               ),
-              Text('Tap the "+" button to create a community.'),
-            ],
-          ),
-        );
-      },
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              alignment: Alignment.center,
+              child: Text(
+                '🏡',
+                style: TextStyle(fontSize: 144),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
